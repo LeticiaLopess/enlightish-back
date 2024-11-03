576a413289bae5f557cd7605749d21cd9c6794b0
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-11-03 02:56:43 -0300

    enlightish initialization

---
+++ src/main/java/com/synchronia/enlightish/Application.java
@@ -0,0 +1,21 @@
+package com.synchronia.enlightish;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+		SpringApplication.run(Application.class, args);
+	}
+
+}
